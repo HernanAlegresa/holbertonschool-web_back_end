883d71d6199fe26970a503ad7370f99161e2cdfc
HernanAlegresa <alegresah@gmail.com>
2024-07-29 09:43:43 -0700

    Updated tasks 5 to 8.

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw new TypeError('Square footage must be a number');
+    this._sqft = sqft;
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
