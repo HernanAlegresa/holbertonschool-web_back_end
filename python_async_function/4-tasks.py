c95a14fb7154e7694a1e0039cb42adbd48c570bb
HernanAlegresa <alegresah@gmail.com>
2024-08-16 19:49:58 -0300

    4 with asyncio, lessgo

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -2,7 +2,7 @@
 """
 Async function that executes multiple tasks.
 """
-
+import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
@@ -16,15 +16,14 @@ async def wait_n_tasks(n: int, max_delay: int) -> List[float]:
     Returns:
         List[float]: List of all delays in ascending order.
     """
-    tasks_list = []
-    delay_list = []
-    # Variable para las tasks y las agrega a la lista de tasks con append
+    task_list = []
     for _ in range(n):
+        # Tarea creada task para task_wait_random y la agregamos a la lista
         task = task_wait_random(max_delay)
-        tasks_list.append(task)
-    # Ejecuta las tasks y guarda los resultados en la lista delays
-    for task in tasks_list:
-        delay = await task
-        delay_list.append(delay)
+        task_list.append(task)
 
-    return sorted(delay_list)
+    # Ejecuta todas las tareas en paralelo y esperamos los resultados
+    delays = await asyncio.gather(*task_list)
+
+    # Lista de delays en orden de menor a mayor.
+    return sorted(delays)
