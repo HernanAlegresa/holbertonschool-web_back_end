ce5766d040622361c835af45cb7da21f34810513
HernanAlegresa <alegresah@gmail.com>
2024-08-21 10:58:41 -0300

    task 4 uodated.

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
 """
-Async function that executes multiple tasks.
+This module defines a function that executes multiple coroutines as tasks.
 """
+
 import asyncio
 from typing import List
-task_wait_random = __import__('3-tasks').task_wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n_tasks(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Args:
-        n (int): Number of times to execute task_wait_random.
-        max_delay (int): Maximum delay time for task_wait_random.
+        n (int): Number of times to execute wait_random.
+        max_delay (int): Maximum delay time for wait_random.
 
     Returns:
         List[float]: List of all delays in ascending order.
     """
-    task_list = []
+    tasks = []
     for _ in range(n):
-        # Tarea creada task para task_wait_random y la agregamos a la lista
-        task = task_wait_random(max_delay)
-        task_list.append(task)
+        task = asyncio.create_task(wait_random(max_delay))
+        tasks.append(task)
+    delays = await asyncio.gather(*tasks)
 
-    # Ejecuta todas las tareas en paralelo y esperamos los resultados
-    delays = await asyncio.gather(*task_list)
-
-    # Lista de delays en orden de menor a mayor.
     return sorted(delays)
