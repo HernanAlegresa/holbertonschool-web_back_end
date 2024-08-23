0b598c17842a376879796a0bd3e0b9b7b07a93d5
HernanAlegresa <alegresah@gmail.com>
2024-08-23 11:13:53 -0300

    Updated task 2, ready to push to intranet.

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Defines a coroutine that measures runtime.
+"""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Executes async_comprehension four times in parallel and measures the total runtime.
+
+    Returns:
+        float: The total runtime in seconds.
+    """
+    start_time = time.perf_counter()  # Medir el tiempo de inicio
+
+    # Ejecutar async_comprehension cuatro veces en paralelo
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+
+    end_time = time.perf_counter()  # Medir el tiempo de finalización
+
+    return end_time - start_time  # Devolver el tiempo total de ejecución
