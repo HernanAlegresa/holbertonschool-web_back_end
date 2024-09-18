493984d3f0ccfc4dd3182253afb7028ba611c718
HernanAlegresa <alegresah@gmail.com>
2024-09-18 18:55:04 -0300

    Corrected spaces on 1.

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 // 1-stdin.js
 
-console.log("Welcome to Holberton School, what is your name?");
+console.log("Welcome to Holberton School, what is your name?\r");
 
-process.stdin.on('data', (data) => {  // captura la entrada del usaurio y lo convierte en string
+process.stdin.on('data', (data) => {  // captura la entrada del usuario y lo convierte en string
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  process.stdin.pause();  // pausa stdin para que no se quede esperando más entrada
+  console.log(`Your name is: ${name}\r`);
+  process.stdin.pause();  // Pausa stdin para que no se quede esperando más entrada
 });
 
-process.on('exit', () => {  // termina el proceso de espera de entrada del usuario y muestra mensaje
-  console.log("This important software is now closing");
+process.on('exit', () => {  // Termina el proceso de espera de entrada del usuario y muestra mensaje
+  console.log("This important software is now closing\r");
 });
