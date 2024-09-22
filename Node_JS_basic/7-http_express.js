bd83b08cb27dea27a2fca5ae5e13a4059fd78a9e
HernanAlegresa <alegresah@gmail.com>
2024-09-22 20:01:34 -0300

    Last update on task 7.

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,39 +4,51 @@ const fs = require('fs').promises;
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-
-app.get('/students', async (req, res) => {
+async function countStudents(path) {
   try {
-    const data = await fs.readFile(process.argv[2], 'utf-8');
-    const rows = data.split('\n').filter(line => line.trim() !== '');
-
-    if (rows.length <= 1) {
-      throw new Error('No data found in the file');
+    const data = await fs.readFile(path, 'utf-8');
+    const lines = data.trim().split('\n').filter(line => line !== '');
+    
+    if (lines.length <= 1) {
+      throw new Error('No valid data found in the file');
     }
 
-    const students = rows.slice(1);
-    let response = `Number of students: ${students.length}\n`;
+    const students = lines.slice(1);  // Ignorar encabezados
+    let totalStudents = 0;
+    const fields = {};
 
-    const fields = students.reduce((acc, student) => {
-      const [firstName, , , field] = student.split(',');
-      if (!acc[field]) {
-        acc[field] = [];
+    students.forEach((line) => {
+      const [firstName, , , field] = line.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
       }
-      acc[field].push(firstName);
-      return acc;
-    }, {});
+      fields[field].push(firstName);
+      totalStudents += 1;
+    });
+
+    let response = `Number of students: ${totalStudents}\n`;
 
     Object.keys(fields).forEach((field) => {
       const studentList = fields[field].join(', ');
       response += `Number of students in ${field}: ${fields[field].length}. List: ${studentList}\n`;
     });
 
-    res.send(response.trim());
+    return response.trim();
   } catch (error) {
-    res.status(500).send('Cannot load the database');
+    throw new Error('Cannot load the database');
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const studentList = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${studentList}`);
+  } catch (error) {
+    res.status(500).send('This is the list of our students\nCannot load the database');
   }
 });
 
