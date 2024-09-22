ca84047e8fdc2d757b8460f6b0184f346f13fe2a
HernanAlegresa <alegresah@gmail.com>
2024-09-22 19:09:05 -0300

    Updated tasks 1 and 2, task 2 changed recently.

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,12 +2,10 @@
 
 console.log("Welcome to Holberton School, what is your name?");
 
-process.stdin.on('data', (data) => {  // captura la entrada del usaurio y lo convierte en string
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  process.stdin.pause();  // pausa stdin para que no se quede esperando más entrada
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data.toString()}`);
 });
 
-process.on('exit', () => {  // termina el proceso de espera de entrada del usuario y muestra mensaje
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  console.log('This important software is now closing\n');
 });
---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8').trim();
+    const rows = data.split('\n').filter(line => line.trim() !== '');
+
+    if (rows.length <= 1) {
+      throw new Error('No data found in the file');
+    }
+
+    const students = rows.slice(1);
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = students.reduce((acc, student) => {
+      const [firstName, , , field] = student.split(',');
+      if (!acc[field]) {
+        acc[field] = [];
+      }
+      acc[field].push(firstName);
+      return acc;
+    }, {});
+
+    Object.keys(fields).forEach((field) => {
+      const studentList = fields[field].join(', ');
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${studentList}`);
+    });
+
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
