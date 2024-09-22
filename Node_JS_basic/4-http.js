0404efc53f132d8d0583e5bbed67ff3f0299b6ee
HernanAlegresa <alegresah@gmail.com>
2024-09-22 19:43:47 -0300

    Updated task 4 corrected.

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,13 +1,15 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
+const port = 1245;
+
+const app = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
+app.listen(port, () => {
+  console.log(`Server is listening in port ${port}`);
 });
 
 module.exports = app;
